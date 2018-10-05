8270a3df75a5cb612c39708d292bdb66c36178ab
DavEat <oned@hotmail.be>
2018-10-06 00:42:04 +0200

    Add doctor door (no anim)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    #region Var
+    public float duration = 10;
+    private bool running;
+    private float _time;
+    #endregion
+    #region MonoFunction
+    private void Update ()
+    {
+		if (_time < Time.time && running)
+        {
+            End();
+        }
+	}
+    #endregion
+    #region Function
+    public virtual void Begin()
+    {
+        running = true;
+        _time = Time.time + duration;
+    }
+    protected virtual void End()
+    {
+        running = false;
+    }
+    #endregion
+}
