28d2e5bdaffcea786d19ca582638d8fae61df023
unknown <gysenjulien@gmail.com>
2018-10-05 23:42:56 +0200

    add blood info

---
+++ Assets/Scripts/BloodInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodInfo
+{
+    public enum BloodType { None, Blood, Platelet};
+    public enum BloodFamily { None, APos, BPos, OPos, ANeg, BNeg, ONeg}
+
+    public BloodType EBloodType = BloodType.None;
+    public BloodFamily EBloodFamily = BloodFamily.None;
+}
