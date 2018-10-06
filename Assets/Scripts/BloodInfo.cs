c25431c883954ec539a3cac15867605d8ee55b8f
DavEat <oned@hotmail.be>
2018-10-06 12:05:14 +0200

    BloodInfo sperate family and rhesus

--- Assets/Scripts/BloodInfo.cs
+++ Assets/Scripts/BloodInfo.cs
@@ -6,13 +6,31 @@ using UnityEngine;
 public class BloodInfo
 {
     public enum BloodType { None, Blood, Platelet, Plasma};
-    public enum BloodFamily { None, APos, BPos, OPos, ANeg, BNeg, ONeg}
+    public enum BloodFamily { None, A, B, AB, O}
+    public enum BloodRhesus { None, neg, pos }
 
     public BloodType type = BloodType.None;
     public BloodFamily family = BloodFamily.None;
+    public BloodRhesus rhesus = BloodRhesus.None;
 
     public bool Equals(BloodInfo info)
     {
-        return family == info.family && type == info.type;
+        return family == info.family && type == info.type && rhesus == info.rhesus;
+    }
+    public bool EqualsType(BloodInfo info)
+    {
+        return type == info.type;
+    }
+    public bool EqualsFamRhe(BloodInfo info)
+    {
+        return family == info.family && rhesus == info.rhesus;
+    }
+    public bool EqualsFam(BloodInfo info)
+    {
+        return family == info.family;
+    }
+    public bool EqualsRhe(BloodInfo info)
+    {
+        return rhesus == info.rhesus;
     }
 }
