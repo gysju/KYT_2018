85a7d9267c00c76737a8423587c290b165308f7a
Dav Eat <oned@hotmail.be>
2018-10-20 21:52:31 +0200

    Add DoTween + start UI anim

---
+++ Assets/Scripts/UIAnimation.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAnimation : MonoBehaviour {
+
+    [SerializeField] private Image _background;
+    [SerializeField] private RectTransform[] _buttons;
+    public float openTime = .7f;
+
+
+    private void Start()
+    {
+        EnterAnim();
+    }
+
+    public void EnterAnim()
+    {
+        _background.CrossFadeAlpha(.5f, .3f, false);
+        OpenMenu(_buttons);
+    }
+
+    private Sequence inTweenOpen;
+    private void OpenMenu(RectTransform[] rects)
+    {
+        inTweenOpen = DOTween.Sequence();
+        for (int i = 0; i < rects.Length;  i++)
+        {
+            inTweenOpen.Join(rects[i].DOAnchorPos(new Vector2(-250, rects[i].anchoredPosition.y), 0));
+            inTweenOpen.Join(rects[i].DOAnchorPos(new Vector2(0, rects[i].anchoredPosition.y), openTime + .1f * i));
+            inTweenOpen.SetDelay(.1f * i);
+        }        
+    }
+
+}
