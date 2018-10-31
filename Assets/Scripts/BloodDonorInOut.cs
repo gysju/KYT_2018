e909857ef1faaaf5bcc7eaccf1cb411fd319bbbe
Dav Eat <oned@hotmail.be>
2018-10-31 12:31:37 +0100

    Add trigger to destroy donor at leave or ragequit

---
+++ Assets/Scripts/BloodDonorInOut.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BloodDonorInOut : MonoBehaviour {
+
+    [SerializeField] private bool _in = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Donor"))
+        {
+            BloodDonor d = other.GetComponent<BloodDonor>();
+            if (_in)
+            {
+                if (d.state == BloodDonor.State.home)
+                    d.state = BloodDonor.State.idle;
+            }
+            else if (d.state == BloodDonor.State.leave || d.state == BloodDonor.State.rageQuit)
+                d.state = d.state;
+        }
+    }
+}
